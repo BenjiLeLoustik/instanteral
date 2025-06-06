0581c457e7278f0d3d7c7c944e21faaa75083c79
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-06-07 00:23:15 +0200

    Modification du controlleur "MainController"

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,11 @@ final class MainController extends AbstractController
     #[Route('/', name: 'main.index')]
     public function index(): Response
     {
-        return $this->render("pages/main/index.html.twig", []);
+        return $this->render("pages/main/index.html.twig", [
+            "about_pseudo"              => "Allistair B.",
+            "about_avatar"              => "",
+            "about_small_description"   => "Lorem ipsum dolor sit amet consectetur. Bibendum donec ultricies eu libero urna eu pellentesque facilisi. Aenean dui feugiat tincidunt interdum ipsum mi rutrum. Eu amet ultrices neque aliquam velit id. A vitae scelerisque nunc interdum risus a egestas. At diam et pharetra suspendisse. Aenean at eu arcu donec nibh",
+        ]);
     }
 
 }
