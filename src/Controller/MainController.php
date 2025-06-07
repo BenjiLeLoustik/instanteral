1f1145b521816f04378274536095cefc632c942d
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-06-07 09:28:34 +0200

    Modification du dossier "Templates" et du fichier "MainController" : Ajout d'articles dans le MainController pour une simulation Ajout des variables du MainController dans le fichier : /templates/pages/main/index.html.twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,10 +12,39 @@ final class MainController extends AbstractController
     #[Route('/', name: 'main.index')]
     public function index(): Response
     {
+
+        $latestArticles = [
+            [
+                "id" => 1,
+                "cover" => "",
+                "title" => "Lorem ipsum sit dolor emet",
+                "smallDescription" => "Lorem ipsum dolor sit amet consectetur. Id lorem id diam pulvinar gravida consequat nec. Libero massa mauris et sapien sagittis mauris. Aenean feugiat tristique pretium ultrices fusce magna. Viverra aliquet nibh mi tortor purus venenatis est urna. Molestie sit ut ultrices morbi et integer...",
+                "date" => [
+                    "added" => "2025-03-20 10:00:00",
+                    "updated" => ""
+                ],
+                "keywords" => array( "mot clé", "mot clé" ),
+                "slug" => "lorem-ipsum-sit-dolor-emet"
+            ],
+            [
+                "id" => 2,
+                "cover" => "",
+                "title" => "Lorem ipsum sit dolor emet 2",
+                "smallDescription" => "Lorem ipsum dolor sit amet consectetur. Id lorem id diam pulvinar gravida consequat nec. Libero massa mauris et sapien sagittis mauris. Aenean feugiat tristique pretium ultrices fusce magna. Viverra aliquet nibh mi tortor purus venenatis est urna. Molestie sit ut ultrices morbi et integer...",
+                "date" => [
+                    "added" => "2025-03-20 10:00:00",
+                    "updated" => "2025-05-10 20:00:00"
+                ],
+                "keywords" => array( "mot clé", "mot clé", "mot clé" ),
+                "slug" => "lorem-ipsum-sit-dolor-emet-2"
+            ]
+        ];
+
         return $this->render("pages/main/index.html.twig", [
             "about_pseudo"              => "Allistair B.",
             "about_avatar"              => "",
             "about_small_description"   => "Lorem ipsum dolor sit amet consectetur. Bibendum donec ultricies eu libero urna eu pellentesque facilisi. Aenean dui feugiat tincidunt interdum ipsum mi rutrum. Eu amet ultrices neque aliquam velit id. A vitae scelerisque nunc interdum risus a egestas. At diam et pharetra suspendisse. Aenean at eu arcu donec nibh",
+            "latestArticles"            => $latestArticles
         ]);
     }
 
